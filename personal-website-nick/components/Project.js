687b80507178f2af8ed58144afa54f47e125c76b
nickambrose7 <nickambrose7@gmail.com>
2023-12-20 00:04:49 -0700

    Got the generic component set up

--- personal-website-nick/components/Project.js
+++ personal-website-nick/components/Project.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,27 +7,35 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function Project() {
+// Create a local dark theme instance
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function Project({ title, description, imageUrl, projectUrl }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <ThemeProvider theme={darkTheme}>
+      <Card sx={{ maxWidth: 1000 }}>
+        <CardMedia
+          sx={{ height: 300 }}
+          image={imageUrl}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" href={projectUrl} target="_blank">Learn More</Button>
+          {/* Add more actions or buttons if needed */}
+        </CardActions>
+      </Card>
+    </ThemeProvider>
   );
 }
